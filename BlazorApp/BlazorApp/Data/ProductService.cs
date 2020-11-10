51fc1d909f17f2666bba150f2caf1fd2412066a9
Emilie <emilie.robert05@gmail.com>
2020-11-10 14:50:30 +0100

    exemple de base

---
+++ BlazorApp/BlazorApp/Data/ProductService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Data
+{
+    public class ProductService
+    {
+        private List<Product> Products;
+
+        public Product SelectedProduct;
+
+        public void ProductSelected(ChangeEventArgs args)
+        {
+            SelectedProduct = (from p in Products
+                               where p.Id == Convert.ToInt32(args.Value.ToString())
+                               select p).FirstOrDefault();
+        }
+
+        public Task<List<Product>> GetProductAsync()
+        {
+            var rnd = new Random();
+
+            return Task.FromResult(Products = new List<Product>
+            {
+                new Product { Id = 1, Name = "Phone", Price = 999.99m + rnd.Next(10,200) },
+                new Product { Id = 2, Name = "Book", Price = 32.59m + rnd.Next(10,200)},
+                new Product { Id = 3, Name = "Car", Price = 15000.42m + rnd.Next(10,200)},
+             });
+
+        }
+    }
+}
---
+++ BlazorApp/BlazorApp/Data/TodoItemService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Data
+{
+    public class TodoItemService
+    {
+        public IList<TodoItem> todos = new List<TodoItem>();
+        public string newTodo;
+
+        public void AddTodo()
+        {
+            if (!String.IsNullOrWhiteSpace(newTodo))
+            {
+                todos.Add(new TodoItem { Title = newTodo, IsDone = false });
+                newTodo = string.Empty;
+            }
+        }
+    }
+}
